8948f35cd339935c650805c8c2fbd1325c5c295d
merveeyenice <meryeyenice@gmail.com>
2024-05-09 19:34:44 +0300

    first commit

---
+++ homepage/scripts/homepage.js
@@ -0,0 +1,6 @@
+output.homepage = {
+    toolbar: "browse_toolbar_container",
+    emptyState: "empty_view_text",
+    bottomBar: "qeb_bottom_bar",
+    newNote: "new_note_button"
+}
---
+++ welcome/scripts/welcome.js
@@ -0,0 +1,7 @@
+output.welcome = {
+    welcomeFragment: "welcome_fragment",
+    welcomeImage: "image",
+    welcomeText: "Capture anything",
+    welcomeDescription: "Make lists, take photos, speak your mind - whatever works for you, works in Keep.",
+    skipWelcomeButton: "Get started"
+}
