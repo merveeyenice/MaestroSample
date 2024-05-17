37db4cf473d06d1bb463edb58cba9de3fdad5ca0
merveeyenice <meryeyenice@gmail.com>
2024-05-17 19:28:38 +0300

    added shortCuts and create new note test  flows

---
+++ appshortcuts/scripts/shortcuts.js
@@ -0,0 +1,6 @@
+output.shortcuts = {
+    newTextNote: "New text note",
+    newList: "New list",
+    newPhotoNote: "New photo note",
+    newAudioNote: "New audio note"
+}
--- createnote/scripts/createnote.js
+++ createnote/scripts/createnote.js
@@ -1,4 +1,6 @@
 output.createnote = {
     noteTitle: "editable_title",
-    noteText: "edit_note_text"
+    noteText: "edit_note_text",
+    bsAddButton: "bs_add_button",
+    browseImgViewSwitcher: "browse_image_view_switcher"
 }
